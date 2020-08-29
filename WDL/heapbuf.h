8c1422998831678c9de6f4a1b1cfa4667709e75a
Andrew Heintz <bindernews@gmail.com>
2020-08-28 21:53:12 -0400

    Add GetCapacity methods to WDL_HeapBuf and WDL_TypedBuf

--- WDL/heapbuf.h
+++ WDL/heapbuf.h
@@ -55,6 +55,7 @@ class WDL_HeapBuf
     void *Get() const { return m_size?m_buf:NULL; } // returns NULL if size is 0
     void *GetFast() const { return m_buf; } // returns last buffer if size is 0
     int GetSize() const { return m_size; }
+    int GetCapacity() const { return m_alloc; }
     void *GetAligned(int align) const {  return (void *)(((UINT_PTR)Get() + (align-1)) & ~(UINT_PTR)(align-1)); }
 
     void SetGranul(int granul) { m_granul = granul; }
@@ -261,6 +262,7 @@ template<class PTRTYPE> class WDL_TypedBuf
     PTRTYPE *Get() const { return (PTRTYPE *) m_hb.Get(); }
     PTRTYPE *GetFast() const { return (PTRTYPE *) m_hb.GetFast(); }
     int GetSize() const { return m_hb.GetSize()/(unsigned int)sizeof(PTRTYPE); }
+    int GetCapacity() const { return m_hb.GetCapacity() / (unsigned int)sizeof(PTRTYPE); }
 
     PTRTYPE *Resize(int newsize, bool resizedown = true) { return (PTRTYPE *)m_hb.Resize(newsize*sizeof(PTRTYPE),resizedown); }
     PTRTYPE *ResizeOK(int newsize, bool resizedown = true) { return (PTRTYPE *)m_hb.ResizeOK(newsize*sizeof(PTRTYPE), resizedown);  }
